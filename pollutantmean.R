a0efc28a8ec8c2757b03d729d714ee2c08fe72bc
Lucas Carpio <lucas.evans.carpio@gmail.com>
2020-11-03 20:56:34 -0400

    Polluteantmean function fixed. First exercise done

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,9 +1,26 @@
-polluantmean <- function(){
-    files <- list.files("./specdata")
+polluantmean <- function(directory, pollutant, id = 1:332){
+    files <- list.files(path= directory, full.names = TRUE , pattern=".csv")
     
-    for (i in files) {
-      data <- read.csv(i)
-    }
+    first <- id[1]
+    
+    for (i in id) {
+        if (i== first) {
+          data <- read.csv(files[i], sep=",", row.names = 1)
+          }
+        else{
+          new_data <- read.csv(files[i], sep=",", row.names = 1)
+          data <- rbind(data, new_data)
+        }
+        
   
-    data <- data.frame(data)
+    }
+   
+    lapply(data, mean, na.rm = TRUE)
+    sapply(data, mean, na.rm = TRUE)
+    result <-colMeans(data, na.rm = TRUE)
+    
+    result[pollutant]
 }
+
+
+
