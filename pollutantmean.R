ecdcd3a22d2d344b8a2594e151e75e57f3a9edc8
Lucas Carpio <lucas.evans.carpio@gmail.com>
2020-11-08 21:05:13 -0400

    Complete an Corr functions added

--- pollutantmean.R
+++ pollutantmean.R
@@ -11,8 +11,6 @@ polluantmean <- function(directory, pollutant, id = 1:332){
           new_data <- read.csv(files[i], sep=",", row.names = 1)
           data <- rbind(data, new_data)
         }
-        
-  
     }
    
     lapply(data, mean, na.rm = TRUE)
@@ -23,4 +21,24 @@ polluantmean <- function(directory, pollutant, id = 1:332){
 }
 
 
-
+complete <- function(directory, ids = 1:332){
+  files <- list.files(path= directory, full.names = TRUE , pattern=".csv")
+  
+  first <- ids[1]
+  
+  for (i in ids) {
+    if (i== first) {
+      data <- read.csv(files[i], sep=",", row.names = 1)
+    }
+    else{
+      new_data <- read.csv(files[i], sep=",", row.names = 1)
+      data <- rbind(data, new_data)
+    }
+  }
+  
+  for (j in id)s {
+    summary_data <- data.frame(id =j, nobs = sum(data, na.rm=TRUE))
+  }
+  
+  summary_data
+}
